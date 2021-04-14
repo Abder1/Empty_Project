024529f8eb05b1ac2e062a9227ecbc776159166b
Abderrahmane <a.benchakroun.ext@wevops.fr>
2021-04-14 17:28:52 +0200

    Exercice reshape

--- demo.js
+++ demo.js
@@ -1,27 +1,56 @@
 'use strict'; 
 (function() {
 
-  function Person(firstName, lastName, age){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.isAdult = function() {return this.age > 18 }
+ /* let person = {
+    name: {
+      first: 'Jim',
+      last: 'Cooper'
+    },
+    age: 29
+  }
+  Object.defineProperty(person, 'fullName',
+  {
+    get: function() {
+      return this.name.first + ' ' + this.name.last;
+    },
+    set: function(val){
+      var nameParts = val.split('-');
+      this.name.first = nameParts[0];
+      this.name.last = nameParts[1];
+    }
+  })
+
+  Object.defineProperty(person, 'age',
+    {configurable: false})
+
+    Object.defineProperty(person, 'age',
+    {writable: false})
+
+  person.age = 40;
+
+  display(person.fullName);
+  person.fullName = "Fred-Jones";
+  display(person.fullName);
+
+  person.level = 3;
+  display(person);
+ 
+
+  function search(logData) {
+    let regex = /ERROR(.*?);/g;
+    let resultat = regex.exec(logData);
+    return resultat;
   }
 
-  function Student(firstName, lastName, age, level){
-    Person.call(this, firstName, lastName, age)
-    this.level=level;
-  }
+  let logData = "INFO:OK;ERROR;INFO:OK:ERROR;";
+  display(search(logData));
 
-  function Stagiaire(firstName, lastName, age, exp){
-    Person.call(this, firstName, lastName, age)
-    this.exp=exp;
-  }
+   */
 
-  Student.prototype = Object.create(Person.prototype);
-  Stagiaire.prototype = Object.create(Person.prototype);
-  let rishi = new Student('Rishi', 'BEN', 21, 5);
-  display(rishi);
-  let lolo = new Stagiaire('Name', 'LOLO', 21, 6);
-  display(lolo);
+  function reshape(num, str){
+      var regex = new RegExp(".{" + num + "}", "g");
+      return str.replace(regex, "$&-");
+    }
+
+    display(reshape(3, "hehejhejhejhejh"));
 })();
